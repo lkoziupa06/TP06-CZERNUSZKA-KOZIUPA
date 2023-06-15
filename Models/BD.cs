2b12226b6bad1f8f8710a7af382b55c365610989
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2023-06-15 08:41:13 -0300

    bd.cs

--- Models/BD.cs
+++ Models/BD.cs
@@ -10,9 +10,47 @@ public static class BD{
     private static string _connectionString = @"Server=localhost;DataBase=Elecciones2023;Trusted_Connection=True;";
 
     public static void AgregarCandidatos(Candidato can){
-
+        string sql = "INSERT INTO Candidatos(IdPartido, Apellido, Nombre, FechaNacimiento, Foto, Postulacion) VALUES (@IdPartido, @Apellido, @Nombre, @FechaNacimiento, @Foto, @Postulacion) ";
+        using (SqlConnection BD = new SqlConnection(_connectionString)){
+            BD.Execute(sql, new {can});
+        }
     }
     public static void EliminarCandidato(int IdCandidato){
-        
+        string sql = "DELETE FROM Candidatos WHERE IdCandidato = @pIdCandidato";
+        using(SqlConnection BD = new SqlConnection(_connectionString)){
+            BD.Execute(sql, new {pIdCandidato = IdCandidato});
+        }
+    }
+    public static Partido VerInfoPartido(int idPartido){
+        Partido partidoBuscado = null;
+        using (SqlConnection BD = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Partidos WHERE IdPartido = @pIdPartido";
+            partidoBuscado = BD.QueryFirstOrDefault<Partido>(sql, new{pIdPartido = idPartido});
+        }
+        return partidoBuscado;
+    }
+    public static Candidato VerInfoCandidato(int IdCandidato){
+        Candidato candidatoBuscado = null;
+        using (SqlConnection BD = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Candidatos WHERE IdCandidato = @pIdCandidato";
+            candidatoBuscado = BD.QueryFirstOrDefault<Candidato>(sql, new{pIdCandidato = IdCandidato});
+        }
+        return candidatoBuscado;
+    }
+    public static List<Partido> ListarPartidos(){
+        List<Partido> listaPartidos = null;
+        using(SqlConnection BD = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Partidos";
+            listaPartidos = BD.Query<Partido>(sql).ToList();
+        }
+        return listaPartidos;
+    }
+        public static List<Candidato> ListarCandidatos(){
+        List<Candidato> listaCandidatos = null;
+        using(SqlConnection BD = new SqlConnection(_connectionString)){
+            string sql = "SELECT * FROM Candidatos";
+            listaCandidatos = BD.Query<Candidato>(sql).ToList();
+        }
+        return listaCandidatos;
     }
 }
